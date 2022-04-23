64728a579df66238fd390f8f51637b0c572843a0
Aditi <aditisuman5480@gmail.com>
2022-04-23 22:06:23 +0530

    que2

---
+++ que2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+  // sum of given nos
+  
+int main()
+{
+    int a = 1;
+    int b = 2;
+    int c = 7;
+    cin >> a >> b >> c;
+
+    cout << a+b+c << endl;
+    return 0;
+}
